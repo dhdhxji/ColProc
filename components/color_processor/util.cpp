80e4e8582c51b6eee161dea434837c38cd417b83
Oleksandr <keysindie@gmail.com>
2021-11-25 23:27:42 +0200

    feat: Update hash function

--- components/color_processor/util.cpp
+++ components/color_processor/util.cpp
@@ -1,8 +1,10 @@
 #include "colproc/util/util.h"
 
-#define A 15845
-#define C 896135
-
 uint32_t hash(uint32_t data, uint32_t out_min, uint32_t out_max) {
-    return ((data * A + C) - out_min) % out_max + out_min;
+    //Ref: https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
+    data = ((data >> 16) ^ data) * 0x45d9f3b;
+    data = ((data >> 16) ^ data) * 0x45d9f3b;
+    data = (data >> 16) ^ data;
+
+    return (data-out_min)%(out_max-out_min) + out_min;
 }
