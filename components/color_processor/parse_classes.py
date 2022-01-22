fa192390ba0d9ad046a262afefb88b89e2d20710
Oleksandr <keysindie@gmail.com>
2022-01-22 19:22:19 +0200

    feat: Implement header traverse

--- components/color_processor/parse_classes.py
+++ components/color_processor/parse_classes.py
@@ -1,11 +1,14 @@
 import argparse
+from pathlib import Path
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-i', help='Inout class directory to analyze')
+    parser.add_argument('-i', help='Input class directory to analyze')
     parser.add_argument('-o', help='Class info json output')
 
     return parser.parse_args()
 
 args = parse_args()
-print(args)
+
+for header in Path(args.i).rglob('*.h'):
+    print(f'Header {header}')
