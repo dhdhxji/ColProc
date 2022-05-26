d1f715b5cc021510ccb2acfc108b638d7cd0506a
Alex <keysindie@gmail.com>
2022-05-26 20:05:41 +0300

    feat: Move console canvas to separate header

--- examples/simple_lua/main.cpp
+++ examples/simple_lua/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <exception>
 #include <string>
+#include "../util/console_canvas.h"
 #include "colproc/runtime/lua_runtime.h"
 #include "colproc/variable/variable_callback.h"
 #include "colproc/util/error.hpp"
@@ -14,58 +15,6 @@
 using namespace std::string_literals;
 
 
-class CanvasConsole: public Canvas
-{
-public:
-    CanvasConsole(size_t w, size_t h): Canvas(w, h) {
-        _canvas = new ColRGB[w*h];
-
-        for(size_t i = 0; i < w*h; ++i) {
-            _canvas[i].r = 0;
-            _canvas[i].g = 0;
-            _canvas[i].b = 0;
-        } 
-    }
-
-    ~CanvasConsole() override {
-        delete[] _canvas;
-    }
-
-    void setPix(size_t x, size_t y, ColRGB col) override {
-        ERR_ASSERT_FALSE(
-            x >= getW() || y >= getH(), 
-            "Out of bounds in setPix("s + std::to_string(x) + ";"s + std::to_string(y) + ")"s
-        );
-        size_t index = y*getW() + x;
-
-        _canvas[index] = col;
-    }
-
-    ColRGB getPix(size_t x, size_t y) const override {
-        ERR_ASSERT_FALSE(
-            x >= getW() || y >= getH(), 
-            "Out of bounds in getPix("s + std::to_string(x) + ";"s + std::to_string(y) + ")"s
-        );
-        size_t index = y*getW() + x;
-
-        return _canvas[index];
-    }
-
-    void display() const override {
-        printf("\033[0;0H");
-        for(size_t y = 0; y < getH(); ++y) {
-            for(size_t x = 0; x < getW(); ++x) {
-                ColRGB col = getPix(x, y);
-                printf("\033[48;2;%d;%d;%dm ", col.r, col.g, col.b);
-                printf("\033[48;2;%d;%d;%dm ", col.r, col.g, col.b);
-            }
-            printf("\n");
-        }
-    }
-
-private:
-    ColRGB* _canvas;
-};
 
 
 
